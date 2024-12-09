231d45722181e80aa56604811b5777c0504694a3
Shubham <89548582+shuvxm@users.noreply.github.com>
2024-12-09 19:16:24 +0530

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,9 @@
+print("Hello")
+# for i in range(1, 11):
+#     print(i,end=" ")
+
+def print_range(start, stop):
+    for i in range(start, stop):
+        print(i, end=" ")
+
+print_range(1, 11)  # prints 1, 2, ..., 10
