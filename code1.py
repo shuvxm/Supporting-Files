6c6224eb972096ea5885c53f4b0c653f0aec6b2a
Shubham <89548582+shuvxm@users.noreply.github.com>
2024-12-25 10:09:33 +0530

    Delete code1.py

--- code1.py
+++
@@ -1,9 +0,0 @@
-print("Hello")
-# for i in range(1, 11):
-#     print(i,end=" ")
-
-def print_range(start, stop):
-    for i in range(start, stop):
-        print(i, end=" ")
-
-print_range(1, 11)  # prints 1, 2, ..., 10
